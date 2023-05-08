02b98bc815e9ecde461caacd00461001d258ea6a
SpGerg <spgerghelp@gmail.com>
2023-05-09 01:00:12 +0500

    Fix

--- Commands/SetUserIdCommand.cs
+++ Commands/SetUserIdCommand.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace SCP1956.Commands
 {
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
     public class SetUserIdCommand : ICommand
     {
         public string Command => "scp1956";
@@ -17,7 +18,9 @@ namespace SCP1956.Commands
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
-            response = "Айди " + arguments[0] + " установлен SCP-1956";
+            response = "Айди " + arguments.Array[0] + " установлен SCP-1956";
+
+            Program.Instance.Config.SCP1956UserId = arguments.Array[0];
 
             return true;
         }
