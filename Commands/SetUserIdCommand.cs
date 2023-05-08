0b0cdf5332d68f1c42ef30357901d692c4db4217
SpGerg <spgerghelp@gmail.com>
2023-05-09 00:59:14 +0500

    Added new command

---
+++ Commands/SetUserIdCommand.cs
@@ -0,0 +1,25 @@
+﻿using CommandSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCP1956.Commands
+{
+    public class SetUserIdCommand : ICommand
+    {
+        public string Command => "scp1956";
+
+        public string[] Aliases => new string[] { };
+
+        public string Description => "";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "Айди " + arguments[0] + " установлен SCP-1956";
+
+            return true;
+        }
+    }
+}
